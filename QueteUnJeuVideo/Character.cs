c49141d30e2771191c39ad43c895b2f79053844d
AnSimonyan <anna.simona1976@gmail.com>
2024-06-17 17:27:20 +0300

    First version

---
+++ QueteUnJeuVideo/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QueteUnJeuVideo
+{
+    public class Character
+    {
+        public string Name { get; }
+        public int AttackForce { get; }
+        public int DefenseForce { get; }
+        public int PointOfLive { get; set; }
+
+        public bool isAlive => PointOfLive > 0;
+
+        public Character(string name, int attackForce, int defenseForce, int pointOfLive)
+
+        {
+            Name = name;
+            AttackForce = attackForce;
+            DefenseForce = defenseForce;
+            PointOfLive = pointOfLive;
+        }
+
+        public void Attack(Character attacked)
+        {
+            attacked.PointOfLive = attacked.PointOfLive + attacked.DefenseForce - AttackForce ;
+        }
+        
+    }
+}
--- QueteUnJeuVideo/Program.cs
+++ QueteUnJeuVideo/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Character char1 = new Character("Attacer",3,2,10);
+            Character char2 = new Character("Attaced",3,4,2);
+
+            while (char1.isAlive && char2.isAlive)
+            {
+                char1.Attack(char2);
+                char2.Attack(char1);
+            }
+            string messageOfWinner;
+
+            if (char1.isAlive)
+            {
+                messageOfWinner = "Winner is: " + char1.Name;
+            }
+            else if (char2.isAlive)
+            {
+                messageOfWinner = "Winner is: " + char2.Name;
+            }
+            else { messageOfWinner = "There is no winners."; }
+            
+
+            Console.WriteLine(messageOfWinner);
         }
     }
 }
