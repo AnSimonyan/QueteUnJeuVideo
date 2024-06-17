f528dc06441a407dba8985b11d97ebb21d978e29
AnSimonyan <anna.simona1976@gmail.com>
2024-06-17 12:44:30 +0300

    Добавьте файлы проекта.

---
+++ QueteUnJeuVideo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QueteUnJeuVideo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
